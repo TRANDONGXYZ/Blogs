f9c71767b84a573013352ffe5d9552c118ae6656
TRANDONGXYZ <trandongxzy@gmail.com>
2021-05-20 18:30:22 +0700

    changed

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+var hamburger, menu;
+
+window.onload = function() {
+    menu = document.getElementById("menu");
+    hamburger = document.getElementById("hamburger");
+    handleNavLink.isOpen = false;
+    hamburger.onclick = handleNavLink;
+}
+
+function handleNavLink() {
+    if (handleNavLink.isOpen == false) {
+        menu.setAttribute("style", "display: flex;");
+        menu.setAttribute("style", "transform: translateX(-100%);");
+        handleNavLink.isOpen = true;
+        console.log(handleNavLink.isOpen);
+    }
+    else {
+        menu.setAttribute("style", "display: none;");
+        menu.setAttribute("style", "transform: translateX(100%);");
+        handleNavLink.isOpen = false;
+        console.log(handleNavLink.isOpen);
+    }
+}
