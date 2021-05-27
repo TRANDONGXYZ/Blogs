a2c107459dc76569ed3668265c6d0e28dfe1690e
TRANDONGXYZ <trandongxzy@gmail.com>
2021-05-28 04:25:03 +0700

    added

--- scrips/script.js
+++ scrips/script.js
@@ -1,13 +1,13 @@
-var hamburger = document.getElementById('hamburger');
-var nav_links = document.getElementById('nav-links');
-var mode = document.getElementById('mode');
-var body = document.getElementsByTagName('body')[0];
-var header = document.getElementsByTagName('header')[0];
-var posts_explores = document.getElementById('posts-explores');
-var format = document.getElementById('format');
-var posts = document.getElementById('posts');
+let hamburger = document.getElementById('hamburger');
+let nav_links = document.getElementById('nav-links');
+let mode = document.getElementById('mode');
+let body = document.getElementsByTagName('body')[0];
+let header = document.getElementsByTagName('header')[0];
+let posts_explores = document.getElementById('posts-explores');
+let format = document.getElementById('format');
+let posts = document.getElementById('posts');
 
-var old_y = window.scrollY, new_y = window.scrollY, pos_scroll = 0;
+let old_y = window.scrollY, new_y = window.scrollY, pos_scroll = 0;
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open');
@@ -18,7 +18,8 @@ mode.addEventListener('click', () => {
     mode.classList.toggle('dark');
     body.classList.toggle('dark');
     header.classList.toggle('dark');
-    posts_explores.classList.toggle('dark');
+    if (posts_explores)
+        posts_explores.classList.toggle('dark');
 
     if (mode.getAttribute('class') == 'dark') {
         let link_img = $('#mode img').attr('src');
@@ -34,43 +35,72 @@ mode.addEventListener('click', () => {
     }
 })
 
-format.addEventListener('click', () => {
-    if (format.getAttribute('class') == 'grid') {
-        let link_img = $('#format div img').attr('src');
-        link_img = link_img.replace('list', 'grid');
-        $('#format p').html('Grid');
-        $('#format div img').attr('src', link_img);
-    }
-    else {
-        let link_img = $('#format div img').attr('src');
-        link_img = link_img.replace('grid', 'list');
-        $('#format p').html('List');
-        $('#format div img').attr('src', link_img);
-    }
-    format.classList.toggle('grid');
-    posts.classList.toggle('grid');
-})
+if (format) {
+    format.addEventListener('click', () => {
+        if (format.getAttribute('class') == 'grid') {
+            let link_img = $('#format div img').attr('src');
+            link_img = link_img.replace('list', 'grid');
+            $('#format p').html('Grid');
+            $('#format div img').attr('src', link_img);
+        }
+        else {
+            let link_img = $('#format div img').attr('src');
+            link_img = link_img.replace('grid', 'list');
+            $('#format p').html('List');
+            $('#format div img').attr('src', link_img);
+        }
+        format.classList.toggle('grid');
+        posts.classList.toggle('grid');
+    })
+}
 
 document.addEventListener('scroll', () => {
-	// var scroll_position = window.scrollY;
-	// if (scroll_position > 250) {
-	// 	header.style.backgroundColor = '#29323c';
-	// } else {
-	// 	header.style.backgroundColor = 'transparent';
-	// }
-
-
     new_y = window.scrollY;
-    if (new_y < old_y && pos_scroll == 0)
-        pos_scroll = old_y;
     if (new_y < old_y) {
-        if (Math.abs(new_y - pos_scroll) < 60)
-            $('header').css('top', (Math.max(0, pos_scroll - 60)) + 'px');
-        else
-            $('header').css('top', new_y + 'px');
+        $('header').css('top', '0');
     }
     else {
-        pos_scroll = 0;
+        $('header').css('top', '-60px');
     }
     old_y = new_y;
 });
+
+
+
+
+
+
+
+
+
+
+
+let prefix = document.getElementById('css-file');
+let address_files = [];
+if (prefix != null) {
+    prefix = prefix.getAttribute('href');
+    prefix = prefix.substring(0, prefix.indexOf('css'));
+    console.log(prefix);
+
+    let common_elements = document.querySelectorAll('*[address-file][src]');
+    for (let i = 0; i < common_elements.length; i++)
+        address_files.push(common_elements[i].getAttribute('address-file'));
+    for (let i = 0; i < common_elements.length; i++) {
+        common_elements[i].setAttribute('src', prefix + address_files[i]);
+        console.log(prefix + address_files[i]);
+    }
+
+    address_files = [];
+    common_elements = document.querySelectorAll('*[address-file][href]');
+    for (let i = 0; i < common_elements.length; i++)
+        address_files.push(common_elements[i].getAttribute('address-file'));
+    for (let i = 0; i < common_elements.length; i++)
+        common_elements[i].setAttribute('href', prefix + address_files[i]);
+}
+
+
+
+
+
+
+
