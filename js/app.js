3ff16eb9a79eabfbfaa0da5efd0ce23b05afe543
TRANDONGXYZ <trandongxzy@gmail.com>
2021-07-01 14:20:00 +0700

    changed

--- js/app.js
+++
@@ -1,7 +0,0 @@
-import links_background_image from "./links-background-image.js";
-import { board_game } from "./board-game.js";
-import { Cell } from "./cell.js";
-import { DELTA_ROW, DELTA_COLUMN } from "./const.js";
-import { init } from "./events.js";
-
-init();
--- js/cell.js
+++
@@ -1,8 +0,0 @@
-export class Cell {
-    constructor(row, col, value, blank = false) {
-        this.row = row;
-        this.col = col;
-        this.value = value;
-        this.blank = blank;
-    }
-}
